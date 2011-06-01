f972e060a4f9d721ef3910913c8f8b2a09e38166
Marius Grigaitis <admin@mar.lt>
2011-06-02 01:43:11 +0300

    gauss3 complete

--- gauss3.rb
+++ gauss3.rb
@@ -3,7 +3,23 @@ a = 0.0
 b = Math::PI/3
 e = 0.00000001
 
-def gauss3(f, a, b, e = 0.001, sn2 = 1.0/0)
+
+def gauss3_solve(f, a, b, e, m = 2, sm2 = 1.0/0)
+    xi = lambda { |i| a + i * (b - a)/m } 
+    sm = 0.upto(m - 1).collect { |i| gauss3(f, xi.call(i), xi.call(i+1)) }.inject {|s, si| s + si}
+
+    diff = (sm2 - sm).abs / (2 ** (2 * 3) - 1)
+
+    print "m = #{m}; Sm = #{sm} diff: #{diff}\n"
+
+    if diff > e
+        gauss3_solve(f, a, b, e, m * 2, sm)
+    else
+        return sm  
+    end
+end
+
+def gauss3(f, a, b)
 
     c1 = 5/9.0
     c2 = 8/9.0
@@ -13,7 +29,7 @@ def gauss3(f, a, b, e = 0.001, sn2 = 1.0/0)
     s2 = 0.0
     s3 = Math.sqrt(0.6)
 
-    g = lambda {|s| f.call(x_transform(a, b, s)) }
+    g = lambda {|s| (b - a).fdiv(2) * f.call(x_transform(a, b, s)) }
 
     result = c1 * g.call(s1) + c2 * g.call(s2) + c3 * g.call(s3)
 end
@@ -22,7 +38,7 @@ def x_transform(a, b, s)
     alpha = (b + a).fdiv(2)
     beta = (b - a).fdiv(2)
 
-    return alpha + beta * s
+    return (b - a).fdiv(2) * s + (a + b).fdiv(2)
 end
 
-print gauss3(f, a, b, e).inspect
+print gauss3_solve(f, a, b, e)
