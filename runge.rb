b49b7ca4252cbc6579f678ec664c191ffd812ef5
Marius Grigaitis <admin@mar.lt>
2011-06-02 03:55:45 +0300

    runge with height diff

--- runge.rb
+++ runge.rb
@@ -3,11 +3,11 @@ require "gnuplot"
 
 f = lambda { |x, u| u * Math.cos(u - x/2.0)**2 + 0.1 * x**2 }
 a = 0
-b = 3
-h = [0.0125]
-#h = [0.1, 0.05, 0.025, 0.0125]
-yi = (0.1..1.5).step(0.1).collect
-#yi = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]
+b = 1
+#h = [0.0125]
+h = [0.1, 0.05, 0.025, 0.0125]
+#yi = (0.1..1.5).step(0.1).collect
+yi = [0.0]
 
 
 def runge(f, a, b, h, yi)
@@ -45,14 +45,14 @@ Gnuplot.open do |gp|
             uh = 1/0.0
             h.each do |h1|
                 xy = runge(f, a, b, h1, cy)
-                uh2 = xy[1].inject { |sum, y| sum + y}
+                uh2 = xy[1][xy[1].size() -1]
                 diff = (uh2 - uh).abs/(2**2 -1)
 
                 plot.data << Gnuplot::DataSet.new(xy) do |ds|
                   ds.with = "lines"
                   ds.linewidth = 1
                   ds.title = ""
-#                  ds.title = "y0: #{cy} h:#{h1} d:(#{diff})"
+                  ds.title = "y0: #{cy} h:#{h1} d:(%.6f)" % diff
                 end
                 uh = uh2
             end
